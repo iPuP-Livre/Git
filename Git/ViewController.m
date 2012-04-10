6cffbaf53f8caca4edf595440786983e562e859b
Jérémy Lagrue <jeremy.lagrue@orange.fr>
2012-04-10 17:09:23 +0200

    modification

--- Git/ViewController.m
+++ Git/ViewController.m
@@ -14,6 +14,7 @@
 {
     [super viewDidLoad];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundImage.png"]];
+    imageView.frame = CGRectMake(40.0, 10.0, 320.0 - 80.0, 460.0 - 20.0);
     [self.view addSubview:imageView];
 }
 
